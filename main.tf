0925f7a52f20fced104738f4ff9a9d29450ca6aa
Mohit Sharma <imoisharma@icloud.com>
2023-09-04 05:38:50 +1000

    add more resources

--- main.tf
+++ main.tf
@@ -2,6 +2,11 @@ resource "aws_acm_certificate" "cert" {
   domain_name       = var.domain_name
   validation_method = var.validation_method
   key_algorithm     = var.key_algorithm
+
+  options {
+    certificate_transparency_logging_preference = var.certificate_transparency_logging_preference ? "ENABLED" : "DISABLED"
+  }
+
   tags = {
     Environment = "test"
   }
--- variables.tf
+++ variables.tf
@@ -4,6 +4,12 @@ variable "domain_name" {
   default     = ""
 }
 
+variable "subject_alternative_names" {
+  description = "Optional) Set of domains that should be SANs in the issued certificate. To remove all elements of a previously configured list, set this value equal to an empty list ([]) or use the terraform taint command to trigger recreation."
+  type        = list(string)
+  default     = []
+}
+
 variable "validation_method" {
   description = " (Optional) Which method to use for validation. DNS or EMAIL are valid. This parameter must not be set for certificates that were imported into ACM and then into Terraform.."
   type        = string
