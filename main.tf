9ee9cbab63424c115e58f3da87f5b945fdb8406e
Mohit Sharma <imoisharma@icloud.com>
2023-09-04 05:31:28 +1000

    add more resources

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "aws_acm_certificate" "cert" {
   domain_name       = var.domain_name
-  validation_method = "DNS"
-
+  validation_method = var.validation_method
+  key_algorithm = var.key_algorithm
   tags = {
     Environment = "test"
   }
--- variables.tf
+++ variables.tf
@@ -1,5 +1,23 @@
 variable "domain_name" {
   description = "(Required) Domain name for which the certificate should be issued"
+  type        = string
+  default     = ""
+}
+
+variable "validation_method" {
+  description = " (Optional) Which method to use for validation. DNS or EMAIL are valid. This parameter must not be set for certificates that were imported into ACM and then into Terraform.."
+  type        = string
+  default     = "DNS"
+
+  validation {
+    condition     = contains(["DNS", "EMAIL", "NONE"], var.validation_method)
+    error_message = "Valid values are DNS, EMAIL or NONE."
+  }
+}
+
+variable "key_algorithm" {
   type = string
-  default = ""
- }
+  description = "(Optional) Specifies the algorithm of the public and private key pair that your Amazon issued certificate uses to encrypt data."
+  default = null
+}
+
